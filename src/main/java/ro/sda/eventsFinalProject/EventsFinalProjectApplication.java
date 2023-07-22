a85e709dae650df312ab6fffd195a19392e0b020
Verzes Ionut Daniel <verzes.ionut@gmail.com>
2023-07-22 11:01:14 +0300

    First EventsFinalProject commit after the project creation and dependency setup and database credentials

---
+++ src/main/java/ro/sda/eventsFinalProject/EventsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package ro.sda.eventsFinalProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/sda/eventsFinalProject/EventsFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.sda.eventsFinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
